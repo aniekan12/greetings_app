715f347e108d1e1a1d05d76afc0dce59d2d80d86
ani <godwin@istrategytech.com>
2023-05-29 21:05:06 +0100

    add logic to greet user with toast

--- app/src/main/java/com/example/greetingsapp/MainActivity.kt
+++ app/src/main/java/com/example/greetingsapp/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.greetingsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editTextName = findViewById<EditText>(R.id.editTextName)
+        val enterButton = findViewById<Button>(R.id.enterButton)
+
+        enterButton.setOnClickListener(View.OnClickListener{
+            Toast.makeText(this@MainActivity,
+                "Welcome ${editTextName.text}",
+                Toast.LENGTH_LONG )
+                .show()
+        })
     }
 }
